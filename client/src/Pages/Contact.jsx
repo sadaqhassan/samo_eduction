aae39ba944db2cced08606f83c0f9f70819eef46
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-04-19 16:57:26 +0300

    contact footer

--- client/src/Pages/Contact.jsx
+++ client/src/Pages/Contact.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Nav from "../Components/Nav";
 import { LocationEdit, Mail, PhoneCall } from "lucide-react";
+import Footer from "../Components/Footer";
 
 const Contact = () => {
   return (
@@ -84,18 +85,7 @@ const Contact = () => {
       </div>
 
       {/* 🔷 MAP / CTA */}
-      <div className="mt-20 bg-[#2f4f4f] text-white rounded-2xl p-10 text-center">
-        <h2 className="text-2xl font-bold mb-4">
-          Need quick help?
-        </h2>
-        <p className="mb-6">
-          Contact our support team and we will respond as soon as possible.
-        </p>
-        <button className="bg-white text-[#2f4f4f] px-6 py-3 rounded-lg font-medium">
-          Chat with Us
-        </button>
-      </div>
-
+      <Footer/>
     </div>
     </div>
   );
