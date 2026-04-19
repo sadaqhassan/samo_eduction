e043f9b0ae0554f9d08f5ca0cbb2c924856c3603
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-04-19 16:57:02 +0300

    footer

---
+++ client/src/Components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <div className="mt-20 bg-[#2f4f4f] text-white rounded-2xl p-10 text-center">
+        <h2 className="text-2xl font-bold mb-4">
+          Need quick help?
+        </h2>
+        <p className="mb-6">
+          Contact our support team and we will respond as soon as possible.
+        </p>
+        <button className="bg-white text-[#2f4f4f] px-6 py-3 rounded-lg font-medium">
+          Chat with Us
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
