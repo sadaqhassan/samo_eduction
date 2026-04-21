04e211fc2b2deff6a7f5b347a33f19fe58f8282e
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-04-21 15:15:26 +0300

    helper

--- server/Configs/Helper.js
+++ server/Configs/Helper.js
@@ -4,7 +4,6 @@ import jwt from 'jsonwebtoken'
 
 
 export const GenerateToken = async (id,role) => {
-    
     const token = await jwt.sign({id:id,role:role},process.env.JWT_SECRET,{expiresIn:"7d"});
-    
+    return token;
 }
