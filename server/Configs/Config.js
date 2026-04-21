3a7f000062e01116c747bb43caadd1787e797981
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-04-21 15:15:20 +0300

    userRoute seed

--- server/Configs/Config.js
+++ server/Configs/Config.js
@@ -2,17 +2,24 @@ import cookieParser from "cookie-parser";
 import dotenv from 'dotenv'
 import express from 'express'
 import cors from 'cors'
-import { connectiondDb } from './Configs/conn.js';
 import { createSuperAdmin } from "../Controllers/seed.controller.js";
+import { connectiondDb } from "./conn.js";
+import { userModel } from "../Models/user.model.js";
+import { userRoute } from "../Routes/user.route.js";
 
-export const configApp = async (app) => {
+export const configApp = async(app) => {
     dotenv.config()
     app.use(express.json());
     app.use(cors())
     app.use(cookieParser())
+    app.use("/api/user",userRoute);
     connectiondDb();
     app.listen(process.env.PORT,()=>{
     console.log("server is running on http://localhost:"+process.env.PORT)
-    createSuperAdmin();
 });
+
+const isExist = await userModel.findOne({role:"admin"})
+    if(!isExist){
+        await createSuperAdmin()
+    }
 }
