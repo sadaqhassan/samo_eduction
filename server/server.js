a4d309162bee26de141aab6bf95398d37f2de4b6
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-04-21 14:25:36 +0300

    some

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 import express from 'express'
 
 import { configApp } from './Configs/Config.js';
+import { userModel } from './Models/user.model.js';
 
 const app = express();
 
 configApp(app);
+
+
+
