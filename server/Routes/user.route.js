3e5481d09fe89a1a7f9e1561ee41a1bc4aeb6de3
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-04-22 10:40:06 +0300

    routes

--- server/Routes/user.route.js
+++ server/Routes/user.route.js
@@ -1,8 +1,22 @@
 import { Router } from "express";
-import { LoginApi } from "../Controllers/user.controller.js";
+import { createUserApi, LoginApi } from "../Controllers/user.controller.js";
+import { isAdmin } from "../MiddleWares/isAdmin.js";
 
 const user = Router();
 
 user.post("/login",LoginApi);
+user.post("/register",isAdmin,createUserApi);
 
 export const userRoute = user;
+
+`// {
+//     "email":"samo",
+//     "name":"sadal",
+//     "password":"samo123",
+//     "role":"teacher"
+// }
+`
+// {
+//     "email" :"admin@gmail.com",
+//     "password":"Samisnha123"
+// }
